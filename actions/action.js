e0cdab525905ee1e88f3332b4b7431d265e6b969
pjhjohn <pjhjohn@gmail.com>
2016-08-01 21:59:02 +0900

    Removed unused files & modules

--- actions/action.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'isomorphic-fetch'
-
-export function fetchPost() {
-  return dispatch => {
-    return fetch('http://jsonplaceholder.typicode.com/posts/', { method: 'GET' })
-      .then(response => response.json())
-      .then(json => dispatch(addPost(json)))
-  }
-}
