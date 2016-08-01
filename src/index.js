469d6b0fb67e3425e2cb1ba8af149ae1ddfcc93a
unknown <>
2016-08-01 18:14:57 +0900

    Set Router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App/App';
+import {Router, Route, broswerHistory, IndexRoute } from 'react-router';
+
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import App from './containers/App/App';
+import Greeting from './components/Greeting/Greeting';
+import Post from './components/Post/Post';
+
+ReactDOM.render((
+    <Router history={broswerHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Greeting} />
+            <Route path="/aaa" component={Post}/>
+            <Route path="/posts/:id" component={Post}/>
+        </Route>
+    </Router>
+), document.getElementById('root'));
