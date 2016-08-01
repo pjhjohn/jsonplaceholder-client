300afd67be21bcb3968b2d148da7ec9c7c6558b8
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-08-01 21:10:45 +0900

    list mockup post
    - PostList added
    - action.js added but it is not working now
      - need to research about Middleware in **redux**
    - packages
      - some of these are might not be needed

--- actions/action.js
+++ actions/action.js
@@ -1,17 +1,6 @@
 import fetch from 'isomorphic-fetch'
 
-/*
-  action types
-*/
-//export const GET_POST = "GET_POST"
-
-
-/*
-* action creator
-*/
-
 export function fetchPost() {
-  console.log("fetch")
   return dispatch => {
     return fetch('http://jsonplaceholder.typicode.com/posts/', { method: 'GET' })
       .then(response => response.json())
