7fd54bc094f471955f2faffbffca6e4619c45e7d
MoonlitAngel <thfqlcdl@gmail.com>
2016-08-10 22:19:21 +0900

    Add meta tag and change favicon using react-helmet

---
+++ src/containers/App/title.js
@@ -0,0 +1,16 @@
+module.exports = {
+  app: {
+    title: 'jsonplaceholder-client',
+    description: 'renewal-react example',
+    head: {
+      titleTemplate: '%s | jsonplaceholder-client',
+      meta: [
+        {name: 'description', content: 'renewal-react example'},
+        {property: 'og:site_name', content: 'jsonplaceholder-client'},
+      ],
+      link: [
+        {"rel": "icon", "href":"http://s.nx.com/s2/portal/nexon/image/nexon/nexon.ico" ,"type":"image/x-icon"},
+      ]
+    }
+  },
+};
