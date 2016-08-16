cebc096c7266c2f3d314ff04d40ccca4666ea296
pjhjohn <pjhjohn@gmail.com>
2016-08-17 00:13:51 +0900

    Changed component&container hierachy for post-related features.
    * App takes only from `container/`
    * Removed PostIndex : unused
    * Use bootstrap button for styling `<button/>` tag
    * Naming : SinglePost -> Post, SINGLE_POST -> POST
    * Naming : PostList -> Posts
    * Naming : Posting -> PostNew, /posting -> /posts/new
    * Separated Posting to component/~/PostForm & container/~/PostNew

---
+++ src/containers/Posts/PostNew.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux'; // glue for redux and react
+import { bindActionCreators } from 'redux';
+
+import { pushPost, loadPost } from './../../actions';
+
+import { PostForm } from './../../components';
+
+class PostNew extends React.Component {
+  constructor(props){
+    super(props);
+    this.props.loadPost();
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit() {
+    var post = {userId: "", id: "", title: "", body: ""};
+    post.userId = document.getElementById("userId").value;
+    post.id = document.getElementById("id").value;
+    post.title = document.getElementById("title").value;
+    post.body = document.getElementById("body").value;
+    this.props.pushPost(post);
+  }
+
+  render() {
+    return (
+      <div className="container text-center">
+        <h1>PostNew Test Page</h1>
+        <p> Please input user-id, id, title and body </p>
+        <PostForm onSubmit={this.onFormSubmit} />
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    post: state.posts
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ pushPost, loadPost }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostNew);
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import { App } from './containers';
+import { App, Posts, Post, PostNew } from './containers';
 
-import { Greeting, PostIndex, Error404, PostSingle, Posting } from './components';
+import { Greeting, Error404 } from './components';
 
 export default(
   <Route path="/" component={App}>
     <IndexRoute component={Greeting} />
     <Route path="home" component={Greeting} />
-    <Route path="posts" component={PostIndex} />
-    <Route path="posts/:id" component={PostSingle} />
-    <Route path="posting" component={Posting} />
+    <Route path="posts" component={Posts} />
+    <Route path="posts/new" component={PostNew} />
+    <Route path="posts/:id" component={Post} />
     <Route path="*" component={Error404} />
   </Route>
 );
