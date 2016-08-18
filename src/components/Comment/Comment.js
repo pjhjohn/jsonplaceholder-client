281ed4c87d31c5018d19cd1859cf8180f3a464f2
lanieerts <lanieerts@gmail.com>
2016-08-18 13:45:01 +0900

    Add inspector and modify comment body.:

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Inspector } from 'react-inspector';
 
 class Comment extends React.Component {
   render() {
+  const data = this.props;
   return (
     <div className="card">
-      <div className="card-title text-center">
-        {this.props.name}({this.props.id})
+      <div className="card-title text-center"> {this.props.name}({this.props.id}, {this.props.email})</div>
+      <div className="card-header text-center"> {this.props.body} </div>
+      <div className="card-body">
+          <Inspector data={data} />
       </div>
-      <div> {this.props.body} </div>
     </div>
     )
   }
