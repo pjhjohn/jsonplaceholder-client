53f6af4cce485e48b281ba9a17fcef60c7fbe7ff
moonlitangel <thfqlcdl@gmail.com>
2016-08-19 17:28:54 +0900

    Modified Carousel.Item img center

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,17 +4,25 @@ import { Carousel } from 'react-bootstrap';
 class SimpleSlider extends React.Component{
   render() {
     return (
+      <div className="text-center">
         <Carousel>
           <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src="https://react-bootstrap.github.io/assets/carousel.png"/>
+            <div className="text-center">
+              <img width={900} height={500} alt="900x500" src="https://react-bootstrap.github.io/assets/carousel.png"/>
+            </div>
           </Carousel.Item>
           <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src="https://react-bootstrap.github.io/assets/carousel.png"/>
+            <div className="text-center">
+              <img width={900} height={500} alt="900x500" src="https://react-bootstrap.github.io/assets/carousel.png"/>
+            </div>
           </Carousel.Item>
           <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src="https://react-bootstrap.github.io/assets/carousel.png"/>
+            <div className="text-center">
+              <img width={900} height={500} alt="900x500" src="https://react-bootstrap.github.io/assets/carousel.png"/>
+            </div>
           </Carousel.Item>
         </Carousel>
+      </div>
     );
   }
 }
