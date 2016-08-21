2c58b818293ea6b1aa3c0727f63868d6e50560e0
KIM, WOOJUNG <gnujoow@users.noreply.github.com>
2016-08-21 17:44:15 +0900

    Added Footer (#30)
    * file added
    
    - Footer.js - react component
    - Footer.scss - stylesheet
    
    * footer styling

---
+++ src/containers/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+          <h1>
+            &lt;footer&gt;
+              I have big foot.
+            &lt;/footer&gt;
+          </h1>
+      </div>
+    );
+  }
+}
+
+export default Footer;
