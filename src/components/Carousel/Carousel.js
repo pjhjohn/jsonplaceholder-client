585aec50fcc8eb61701776c24a9b20fffdcb697f
moonlitangel <thfqlcdl@gmail.com>
2016-08-22 17:01:24 +0900

    Change import classname 'Carousel as BootstrapCarousel'
    Duplicate declaration "Carousel"

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel as BootstrapCarousel } from 'react-bootstrap';
 
 class Carousel extends React.Component{
   render() {
     return (
       <div className="text-center">
-        <Carousel>
-          <Carousel.Item>
+        <BootstrapCarousel>
+          <BootstrapCarousel.Item>
             <div className="text-center">
               <img width={900} height={500} alt="900x500" src="https://react-bootstrap.github.io/assets/carousel.png"/>
             </div>
-          </Carousel.Item>
-          <Carousel.Item>
+          </BootstrapCarousel.Item>
+          <BootstrapCarousel.Item>
             <div className="text-center">
               <img width={900} height={500} alt="900x500" src="https://react-bootstrap.github.io/assets/carousel.png"/>
             </div>
-          </Carousel.Item>
-          <Carousel.Item>
+          </BootstrapCarousel.Item>
+          <BootstrapCarousel.Item>
             <div className="text-center">
               <img width={900} height={500} alt="900x500" src="https://react-bootstrap.github.io/assets/carousel.png"/>
             </div>
-          </Carousel.Item>
-        </Carousel>
+          </BootstrapCarousel.Item>
+        </BootstrapCarousel>
       </div>
     );
   }
