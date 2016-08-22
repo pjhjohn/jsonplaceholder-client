c82af045f3268e6557a3c0419c9762e886adc05b
lanieerts <lanieerts@gmail.com>
2016-08-22 22:18:33 +0900

    start/end/limit to Posts. Separate Initial & Additional Data Fetch Action (#33)
    * Let state contain some status required to post list
    * Removed componentDidMount().
    * Fixed #34 through modifying mapping function in posts
    * Fixed #25 through separating actions
    * Produces #36

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -3,24 +3,22 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router'
 
-import { readPosts } from './../../actions';
+import { readPosts, readMorePosts } from './../../actions';
 
 import { PostItem } from './../../components';
 
 class Posts extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.props.readPosts(0);
-    this.onReadPosts = this.onReadPosts.bind(this);
+  componentWillMount() {
+    if(!this.props.initialDataLoaded)
+      this.props.readPosts(0);
   }
 
   onReadPosts(postLength) {
-    this.props.readPosts(postLength);
+    this.props.readMorePosts(postLength);
   }
 
   render() {
-    let startId = this.props.posts.length;
+    console.log(this.props);
     return (
       <div>
         <h1 className="text-center"> {window.location.pathname} </h1>
@@ -30,15 +28,13 @@ class Posts extends React.Component {
         </Link>
 
         <div className="row">
-        { this.props.posts.map((post) =>
-          <Link to={"/posts/" + post.id} >
-            <PostItem key={post.id} {...post} />
-          </Link>
+        {
+          this.props.posts.map((post) => <Link to={"/posts/" + post.id} key={post.id} ><PostItem {...post} /></Link>
         )}
         </div>
 
         <button className="col-md-12 btn btn-default"
-          onClick={() => this.onReadPosts(startId)}>
+          onClick={() => this.onReadPosts(this.props.endPostId)}>
           로오딩
         </button>
       </div>
@@ -48,12 +44,15 @@ class Posts extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    posts: state.post.list
+    posts: state.post.list,
+    startPostId: state.post.start,
+    endPostId: state.post.end,
+    initialDataLoaded: state.post.initialDataLoaded
   };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ readPosts }, dispatch);
+  return bindActionCreators({ readPosts, readMorePosts }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Posts);
