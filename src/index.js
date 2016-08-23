e029e93c46b642db92955dbec56eadbe8e9b066b
moonlitangel <thfqlcdl@gmail.com>
2016-08-23 16:22:04 +0900

    Add Redux-Devtools
    Reference-https://github.com/gaearon/redux-devtools/blob/master/docs/Walkthrough.md

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
 
-import reducers from './reducers';
+import configustore from './store/configuerStore';
 import routes from './routes';
 
 import './index.scss';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const store = configustore();
 
 ReactDOM.render((
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
 ), document.getElementById('root'));
