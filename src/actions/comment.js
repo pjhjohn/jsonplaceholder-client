39ed49eafa991b43483a728eb7c7ae5ac5f4f340
lanieerts <lanieerts@gmail.com>
2016-08-23 19:12:57 +0900

    Add comment form in single post.

--- src/actions/comment.js
+++ src/actions/comment.js
@@ -1,11 +1,31 @@
 import { api } from  './api';
 
 export const READ_COMMENTS = 'READ_COMMENTS';
+export const CREATE_COMMENT = 'CREATE_COMMENTS';
+
+export const DEFAULT_LIMIT_COMMENT = 3;
 
 export function readComments(postId){
   const request = api.get(`/comments?postId=${postId}`);
   return {
     type: READ_COMMENTS,
+    payload: request
+  }
+}
+
+export function readMoreComments(postId, startId){
+  const request = api.get(`posts/${postId}/comments?_start=${startId}&_limit=${DEFAULT_LIMIT_COMMENT}`);
+  return {
+    type: READ_COMMENTS,
+    payload: request
+  }
+}
+
+export function createComment(comment){
+  console.log(comment);
+  let request = api.post(`/posts/${comment.postId}/comments`, comment, {headers : {'Content-Type': 'application/json'}});
+  return {
+    type: CREATE_COMMENT,
     payload: request
   }
 }
--- src/reducers/comment.js
+++ src/reducers/comment.js
@@ -1,11 +1,13 @@
-import { READ_COMMENTS } from './../actions';
+import { READ_COMMENTS, CREATE_COMMENT } from './../actions';
 
 const INITIAL_COMMENT_STATE = { list: [] };
 
 export default function (state = INITIAL_COMMENT_STATE, action) {
   switch(action.type) {
     case READ_COMMENTS:
-      return {...state, list: action.payload.data };
+      return {...state, list: state.list.concat(...action.payload.data) };
+    case CREATE_COMMENT:
+      return state;
     default:
       return state;
   }
