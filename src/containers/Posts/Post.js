944a925bc0f82fcf27983b4e8b39405fab570eaf
lanieerts <lanieerts@gmail.com>
2016-08-24 20:34:24 +0900

    Created comment functionality to post detail (#39)

--- src/containers/Posts/Post.js
+++ src/containers/Posts/Post.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router'
 
-import { readPost, readComments } from './../../actions';
+import { readPost, readComments, createComment, readMoreComments } from './../../actions';
 
 import { PostDetail, Comment } from './../../components';
 
@@ -14,7 +15,18 @@ class Post extends React.Component {
     this.props.readComments(this.props.params.id);
   }
 
+  onSubmit = (props) => {
+    props.postId = this.props.params.id;
+    let postLength = this.props.comments.length;
+    let nowState = this;
+    this.props.createComment(props)
+      .then(function () {
+        nowState.props.readMoreComments(props.postId, {'_start' : postLength});
+      });
+  }
+
   render() {
+    const {fields: { name, email, body}, handleSubmit} = this.props;
     return (
       <div>
         <Helmet title={this.props.post.title} />
@@ -28,10 +40,34 @@ class Post extends React.Component {
           <Comment key={comment.id} {...comment} />
         )}
         </div>
+        <div>
+          <h1> Comment </h1>
+          <p> You can add comment about this post! (Enter your name, email and body) </p>
+          <form onSubmit={handleSubmit(this.onSubmit)}>
+            <div className="form-group">
+              <label> Name </label>
+              <input type="form-control" placeholder="Name" {...name} />
+            </div>
+            <div className="form-group">
+              <label> Email </label>
+              <input type="form-control email" placeholder="Email" {...email} />
+            </div>
+            <div className="form-group">
+              <label> Body </label>
+              <input type="form-control" placeholder="body" {...body} />
+            </div>
+          <button type="Submit"> Comment Submit </button>
+          </form>
+        </div>
       </div>
     );
   }
 }
+
+Post = reduxForm({
+    form: 'CommentsNewForm',
+    fields: ['name', 'email', 'body']
+})(Post);
 
 function mapStateToProps(state) {
   return {
@@ -41,7 +77,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ readPost, readComments }, dispatch);
+  return bindActionCreators({ readPost, readComments, readMoreComments, createComment }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Post);
