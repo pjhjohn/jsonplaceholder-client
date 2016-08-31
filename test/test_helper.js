6ee56040e14c48d5b8ad992654fd97b1f1dae666
woojung <gnujoow.kim@gmail.com>
2016-09-01 05:05:37 +0900

    simulate helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -5,7 +5,8 @@ import ReactDom from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import chaiJquery from 'chai-jquery';
 
 import reducers from './../src/reducers';
 
@@ -22,5 +23,15 @@ function renderComponent(ComponentClass, props, state) {
 
   return $(ReactDOM.findDOMNode(componentInstance));
 }
+
+$.fn.simulate = function(eventName, value) {
+  if(value){
+    this.val(value);
+  }
+
+  TestUtils.Simulate[eventName](this[0]);
+}
+
+chaiJquery(chai, chai.util, $);
 
 export { renderComponent, expect };
