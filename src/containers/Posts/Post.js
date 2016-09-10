e66354e4172e14ab556181fda50c1c1514b73fa5
pjhjohn <pjhjohn@gmail.com>
2016-09-11 04:56:43 +0900

    [Close #35] Migrated redux-form to v6.0.2 with material-ui
    - Beautified library import
    - Now `connect` is required even if using `reduxForm`
      (Not taken care of by redux-form)
    - Re-added comment form in `/posts/:id` page
    - Add toast for comment creation
    - Reversed order of displaying comment
    - Redirect to created post page after writing a post (#52)
    - Material Design for forms with material-ui
    - Use pristine & submitting flags from redux-form for disabling button

--- src/containers/Posts/Post.js
+++ src/containers/Posts/Post.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { connect } from 'react-redux';
-import { reduxForm } from 'redux-form';
-import { bindActionCreators } from 'redux';
+import { Link } from 'react-router';
 import { Row, Col } from 'react-grid-system';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Field, reduxForm } from 'redux-form';
+import { TextField } from 'redux-form-material-ui';
+
+import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import CircularProgress from 'material-ui/CircularProgress';
-import { Card, CardHeader, CardText } from 'material-ui/Card';
-import { Link } from 'react-router'
+import { Card, CardHeader, CardTitle, CardText, CardActions } from 'material-ui/Card';
 
-import { readPost, readComments, createComment, readMoreComments } from './../../actions';
+import { readPost, readComments, createComment, readMoreComments, notify } from './../../actions';
 
 import { PostDetail, Comment } from './../../components';
 
@@ -25,7 +29,6 @@ class Post extends React.Component {
   };
 
   state = {
-    disabled: false,
     initializedPost: false,
     initializedComments: false
   };
@@ -37,15 +40,16 @@ class Post extends React.Component {
       .then(()=>this.setState({initializedComments: true}));
   };
 
-  onSubmit = (props) => {
-    this.setState({ 'disabled' : true });
-    props.postId = this.props.params.id;
-    this.props.createComment(props)
-      .then(() => this.props.readMoreComments({'_start' : this.props.comments.length, 'postId' : this.props.params.id}))
-      .then(() => this.setState({ 'disabled' : false }));
+  onSubmit = (data) => {
+    data.postId = this.props.params.id;
+    this.props.createComment(data).then((response) => {
+      this.props.notify(`comment`, response.payload.status);
+      this.props.readMoreComments({'_start' : this.props.comments.length, 'postId' : this.props.params.id});
+    });
   };
 
   render() {
+    const { handleSubmit, pristine, submitting } = this.props;
     const progress = (<CircularProgress style={{textAlign:`center`, width:`100%`}} />);
     if(!this.state.initializedPost || !this.state.initializedComments) return (progress);
     return (
@@ -86,70 +90,56 @@ class Post extends React.Component {
 
         <Row>
           <Col md={12}>
+            <Card>
+              <CardTitle title={`Comment`} />
+              <form onSubmit={handleSubmit(this.onSubmit)}>
+                <CardText>
+                  <Row>
+                    <Col md={6}>
+                      <Field fullWidth={true} component={TextField} name="email" floatingLabelText="Email"/>
+                    </Col>
+                    <Col md={6}>
+                      <Field fullWidth={true} component={TextField} name="name" floatingLabelText="Name"/>
+                    </Col>
+                    <Col md={12}>
+                      <Field fullWidth={true} component={TextField} name="body" floatingLabelText="Body"/>
+                    </Col>
+                  </Row>
+                </CardText>
+                <CardActions style={{ paddingRight: 0, textAlign: 'right' }}>
+                  <FlatButton type="button" label="Cancel" onTouchTap={() => this.context.router.push(`/posts/${this.props.post.id}`)} />
+                  <FlatButton type="submit" label="Submit" primary={true} disabled={pristine||submitting} />
+                </CardActions>
+              </form>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={12}>
           { this.props.comments.map((comment) =>
             <Comment key={comment.id} {...comment} />
           )}
           </Col>
         </Row>
-
-        {/*<Panel header={`${window.location.pathname}/comments/new`} bsStyle="default">*/}
-          {/*<Form horizontal onSubmit={handleSubmit(this.onSubmit)}>*/}
-          {/**/}
-            {/*<FormGroup controlId="formHorizontalEmail">*/}
-              {/*<Col componentClass={ControlLabel} sm={2}>*/}
-                {/*Email*/}
-              {/*</Col>*/}
-              {/*<Col sm={10}>*/}
-                {/*<FormControl type="email" placeholder="Email" {...email} />*/}
-              {/*</Col>*/}
-            {/*</FormGroup>*/}
-            {/**/}
-            {/*<FormGroup controlId="formHorizontalName">*/}
-              {/*<Col componentClass={ControlLabel} sm={2}>*/}
-                {/*Name*/}
-              {/*</Col>*/}
-              {/*<Col sm={10}>*/}
-                {/*<FormControl type="text" placeholder="Name" {...name} />*/}
-              {/*</Col>*/}
-            {/*</FormGroup>*/}
-            {/**/}
-            {/*<FormGroup controlId="formHorizontalBody">*/}
-              {/*<Col componentClass={ControlLabel} sm={2}>*/}
-                {/*Body*/}
-              {/*</Col>*/}
-              {/*<Col sm={10}>*/}
-                {/*<FormControl componentClass="textarea" type="text" placeholder="Body" {...body} />*/}
-              {/*</Col>*/}
-            {/*</FormGroup>*/}
-            {/**/}
-            {/*<FormGroup style={{marginBottom: 0}}>*/}
-              {/*<Col smOffset={2} sm={10}>*/}
-                {/*<Button style={{width: `100%`}} type="submit" bsStyle="default" disabled={this.state.disabled}>*/}
-                  {/*Submit*/}
-                {/*</Button>*/}
-              {/*</Col>*/}
-            {/*</FormGroup>*/}
-          {/*</Form>*/}
-        {/*</Panel>*/}
       </div>
     );
   }
 }
 
-// Post = reduxForm({
-//   form: 'CommentsNewForm',
-//   fields: ['name', 'email', 'body']
-// })(Post);
+Post = reduxForm({
+  form: 'CommentsNewForm'
+})(Post);
 
 function mapStateToProps(state) {
   return {
     post: state.post.detail,
-    comments: state.comment.list
+    comments: state.comment.list.slice().reverse()
   };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ readPost, readComments, readMoreComments, createComment }, dispatch);
+  return bindActionCreators({ readPost, readComments, readMoreComments, createComment, notify }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Post);
